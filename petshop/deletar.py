a67ae9c011ab7847cbcb41c05baee91d97f51d88
MirelaPedro <156092133+MirelaPedro@users.noreply.github.com>
2024-11-28 10:23:39 -0300

    arquivo deletar

---
+++ petshop/deletar.py
@@ -0,0 +1,32 @@
+import mysql
+import mysql.connector
+from tkinter import messagebox
+
+conexao_banco = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    database = 'clinica_petshop'
+)
+
+cursor = conexao_banco.cursor()
+
+def verificar(tabela, condicao):
+    select= f'select* from {tabela} where {condicao}'
+    cursor.execute(select)
+    dados = cursor.fetchall()
+    return dados
+
+
+def deletar(tabela, id):
+
+    dados = verificar(tabela, f'id = {id}')
+
+    if len(dados) > 0:
+        delete  = f'delete from {tabela} where id = {id}'
+        cursor.execute(delete)
+        conexao_banco.commit()
+    else:
+        messagebox.showinfo('ID inexistente!', "Esse id não existe!")
+
+
