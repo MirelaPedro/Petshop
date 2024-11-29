bb6ab35100331ea2b5f91e60da1277b9b3509070
MirelaPedro <156092133+MirelaPedro@users.noreply.github.com>
2024-11-29 11:44:41 -0300

    pesquisar - só falta formatar e melhorar o print

--- petshop/pesquisar.py
+++ petshop/pesquisar.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+from tkinter import *
 import mysql
 import mysql.connector
 
@@ -19,34 +21,42 @@ def pesquisar(tabela, frame, checkboxes, termo):
 
     if tabela == 'funcionario':
         colunas = colunas_funcionario
-        info = 'funcionario.id, funcionario.nome, funcionario.telefone, funcionario.cargo, funcionario.salario, funcionario.cep, nome.animal, banho.tipo, banho.data, banho.horário  from funcionario, animal, banho where banho.idfuncionario = funionario.id and banho.idanimal = animal.id'
+        info = 'funcionario.id, funcionario.nome, funcionario.telefone, funcionario.cargo, funcionario.salario, funcionario.cep, animal.nome, banho.tipo, banho.data, banho.horário  from funcionario, animal, banho where banho.idfuncionario = funcionario.id and banho.idanimal = animal.id '
 
     elif tabela == 'proprietario':
         colunas = colunas_proprietario
-        info = 'proprietario.id, proprietario.nome, proprietario.telefone, proprietario.cep, animal.id, animal.nome, banho.idanimal, banho.tipo, banho.data, banho.horário from proprietario, animal, banho where animal.idproprietario = proprietario.id and banho.idanimal = animal.id'
+        info = 'proprietario.id, proprietario.nome, proprietario.telefone, proprietario.cep, animal.id, animal.nome, banho.idanimal, banho.tipo, banho.data, banho.horário from proprietario, animal, banho where animal.idproprietario = proprietario.id and banho.idanimal = animal.id '
 
     elif tabela == 'animal':
         colunas = colunas_animal
-        info = 'animal.id, animal.nome, animal.especie, animal.raca, animal.peso, animal.altura, animal.idade, animal.cor, proprietario.id, proprietaio.nome, banho.tipo, banho.data, banho.horário  from animal, proprietario, banho where animal.idproprietario = proprietario.id and banho.idanimal = animal.id'
+        info = 'animal.id, animal.nome, animal.especie, animal.raca, animal.peso, animal.altura, animal.idade, animal.cor, proprietario.id, proprietaio.nome, banho.tipo, banho.data, banho.horário  from animal, proprietario, banho where animal.idproprietario = proprietario.id and banho.idanimal = animal.id '
 
     elif tabela == 'banho':
         colunas = colunas_banho
-        info = 'banho.id, banho.horário, banho.data, banho.tipo, banho.valor, banho.idanimal, banho.idfuncionario, animal.nome, funcionario.nome from banho, animal, funcionario where banho.idfuncionario = funionario.id and banho.idanimal = animal.id'
+        info = 'banho.id, banho.horário, banho.data, banho.tipo, banho.valor, banho.idanimal, banho.idfuncionario, animal.nome, funcionario.nome from banho, animal, funcionario where banho.idfuncionario = funionario.id and banho.idanimal = animal.id '
     
 
-    if checkboxes[6] == 1:
+    print(checkboxes)
+    condicao =''
+    if checkboxes[6] == True:
         for coluna in colunas:
             condicao += f'and funcionario.{coluna} like "%{termo}%" '
     else:
-        for i in checkboxes:
-            if checkboxes == 1:
-                condicao += f'and funcionario.{coluna} like "%{termo}%" '
+        for index, i in enumerate(checkboxes):
+            if i == True:
+                    condicao += f'and funcionario.{colunas[index]} like "%{termo}%" '
         
     select = f'select {info} {condicao}'
+    print(select)
 
     cursor.execute(select)
     dados = cursor.fetchall()
 
     print(dados)
+
+    lb_dados = tk.Label(frame, text=dados)
+    lb_dados.pack()
+
+
 
 
